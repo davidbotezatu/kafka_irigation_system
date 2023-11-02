18ea62142755c2e4e7fd1f8f1e991218fb3cca1e
David Botezatu <botezatu.david91@gmail.com>
2023-11-02 14:13:47 +0100

    Producer creation

---
+++ src/main/java/producers/ProducerConfigFactory.java
@@ -0,0 +1,37 @@
+package producers;
+
+import org.apache.kafka.clients.producer.*;
+import java.util.Properties;
+
+public class ProducerConfigFactory {
+    private final Producer<String, String> producer;
+
+    public ProducerConfigFactory(String bootstrapServers) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        this.producer = new KafkaProducer<>(props);
+        System.out.println("producer created");
+    }
+
+    public void sendSensorData(String topic, String key, String value) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+
+        producer.send(record, (metadata, e) -> {
+            if (e != null) {
+                e.printStackTrace();
+            } else {
+                System.out.println("Topic: " + metadata.topic());
+                System.out.println("Partition: " + metadata.partition());
+                System.out.println("Offset: " + metadata.offset());
+            }
+        });
+    }
+
+    public void closeProducer() {
+        producer.close();
+    }
+}
---
+++ src/main/java/producers/ReservoirProducer.java
@@ -0,0 +1,39 @@
+package producers;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReservoirProducer {
+    private static final String TOPIC = "water-levels";
+    private static final String DATA_DIRECTORY = System.getProperty("user.dir") + File.separator + "test_data";
+
+    public static void startReservoirProducer(ProducerConfigFactory producer) {
+        Pattern fileNamePattern = Pattern.compile("test_data_reservoir_(\\d+)_(top|bottom).txt");
+        File[] files = new File(DATA_DIRECTORY).listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                String fileName = file.getName();
+                String filePath = DATA_DIRECTORY + File.separator + fileName;
+                Matcher matcher = fileNamePattern.matcher(fileName);
+
+                if (matcher.matches()) {
+                    String reservoirNumber = matcher.group(1);
+                    String sensorPosition = matcher.group(2);
+                    String key = "Reservoir" + reservoirNumber + "_" + sensorPosition;
+
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+                        while (reader.readLine() != null) {
+                            String value = reader.readLine();
+                            producer.sendSensorData(TOPIC, key, value);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
