170202c5e1e46973f64f9510025fd5ec3952d11b
David Botezatu <botezatu.david91@gmail.com>
2023-11-16 14:33:16 +0100

    Create test data for parcels

--- producer/src/main/java/Main.java
+++ producer/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import config.ProducerConfigFactory;
 import producers.ReservoirProducer;
+import utils.ParcelsTestDataCreation;
 import utils.TestDataCreation;
 
 import java.nio.file.Path;
@@ -9,14 +10,16 @@ import java.util.Properties;
 public class Main {
     private static final String BOOTSTRAP_SERVERS = "localhost:19092,localhost:29092,localhost:39092";
     private static final String TOPIC = "water-levels";
-    private static final long SLEEP_DURATION = 1000; // Sleep duration between messages
+    private static final long SLEEP_DURATION = 1000; // Sleep duration between messages - for testing purposes
     private static final Path DATA_DIRECTORY = Paths.get(System.getProperty("user.dir"), "test_data");
+    private static final Path PARCEL_DATA_DIR = Paths.get(System.getProperty("user.dir"), "parcels_test_data");
 
     public static void main(String[] args) {
         // Create test data
         TestDataCreation.createData(100);
+        ParcelsTestDataCreation.createData(100);
 
-        Properties producerProps = ProducerConfigFactory.createProducerConfig(BOOTSTRAP_SERVERS);
+        Properties producerProps = ProducerConfigFactory.createProducerConfig(BOOTSTRAP_SERVERS, 1);
 
         // Start one producer for each reservoir
         for (int i = 1; i <= 7; i++) {
@@ -31,6 +34,11 @@ public class Main {
                     producerProps,
                     SLEEP_DURATION
             )).start();
+        }
+
+        //Start one producer for each parcel
+        for (int i = 1; i <= 3; i++) {
+            Path parcelData = PARCEL_DATA_DIR.resolve("parcel_" + i + ".txt");
         }
     }
 }
--- producer/src/main/java/config/ProducerConfigFactory.java
+++ producer/src/main/java/config/ProducerConfigFactory.java
@@ -6,12 +6,20 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 public class ProducerConfigFactory {
-    public static Properties createProducerConfig(String bootstrapServers) {
+    public static Properties createProducerConfig(String bootstrapServers, int type) {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        //use type 1 for reservoir and type 2 for parcel producers
+        if (type == 1) {
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        } else if (type == 2) {
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        }
+
         return props;
     }
 }
